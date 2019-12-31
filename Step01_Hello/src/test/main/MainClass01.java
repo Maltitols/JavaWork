84e80d3980d9e730254350ab859765552b2b41b7
leechangho <maltitol7090@gmail.com>
2019-12-31 10:19:02 +0900

    test.mypac 생성

--- Step01_Hello/src/test/main/MainClass01.java
+++ Step01_Hello/src/test/main/MainClass01.java
@@ -1,8 +1,12 @@
 package test.main;
 
+import test.mypac.Car;
+
 public class MainClass01 {
 	public static void main(String[] args) {
 		System.out.println("으악");
 		System.out.println("으엑");
+		Car car1=new Car();
+		car1.drive();
 	}
 }
